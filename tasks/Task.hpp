823d8ede033fe9483d7b09e0f2f9517514f47396
Sylvain Joyeux <sylvain.joyeux@tidewise.io>
2025-09-12 15:18:38 -0300

    fix: default value of m_lut_center_duty_cycle
    In C++ only, so never used (overwritten in configureHook)

--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -34,7 +34,7 @@ namespace thrusters_blue_robotics_t500 {
     protected:
     private:
         // Properties
-        std::uint32_t m_no_actuation_pwm_command = 1500;
+        std::uint32_t m_no_actuation_pwm_command = 1500000;
         std::uint32_t m_lut_center_duty_cycle = 1500000;
         base::JointState::MODE m_cmd_in_mode;
 
